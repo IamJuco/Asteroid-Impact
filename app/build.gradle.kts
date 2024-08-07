4af07a00251675a8b10f5022dbac1b343a9e9cd7
Juseonghyun <wntjdgus99@naver.com>
2024-08-07 18:52:11 +0900

    chore: navigation 빌드 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,10 @@ dependencies {
     implementation (libs.okhttp)
     implementation (libs.logging.interceptor)
 
+    // Navigation
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
