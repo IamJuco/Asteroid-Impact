1b0de3ea1ec93c7d567bdedf23a9b26ba1145d22
Juseonghyun <wntjdgus99@naver.com>
2024-08-08 01:05:35 +0900

    chore: 테스트 파일명 변경
    공식 앱 이름인 "asteroid_impact"으로 변경

--- app/src/androidTest/java/com/example/asteroid_impact/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/asteroid_impact/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.nbc_assignmenttest", appContext.packageName)
+        assertEquals("com.example.asteroid_impact", appContext.packageName)
     }
 }
