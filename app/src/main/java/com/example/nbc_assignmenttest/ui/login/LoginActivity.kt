9a2a9c3b0e9d623eb4121ce3f7ba6324fd44b06a
Juseonghyun <wntjdgus99@naver.com>
2024-08-07 20:29:30 +0900

    feat: LoginActivity 추가 및 viewBinding 설정

---
+++ app/src/main/java/com/example/nbc_assignmenttest/ui/login/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.nbc_assignmenttest.ui.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.example.nbc_assignmenttest.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        uiSetting()
+
+    }
+
+    private fun uiSetting() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
+    }
+}
