107bc5b998e1988e26d934f2258384e69e481b07
Juseonghyun <wntjdgus99@naver.com>
2024-08-08 14:31:50 +0900

    feat: Nasa Api키 추가

--- app/src/main/java/com/example/asteroid_impact/Constants.kt
+++ app/src/main/java/com/example/asteroid_impact/Constants.kt
@@ -1,6 +1,8 @@
 package com.example.asteroid_impact
 
 object Constants {
-    // Nasa
+    // Nasa Base Url
     const val NASA_BASE_URL = "https://api.nasa.gov/"
+    // Nasa Api Key
+    const val NASA_API_KEY  = "hQb8FEamBoAD0gLuzK4tiCWtjowrmRELN3oYb2Br"
 }
