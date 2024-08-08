6e3101da8e75125ed65b25633b8b58f6969a2cf4
Juseonghyun <wntjdgus99@naver.com>
2024-08-08 21:19:30 +0900

    feat: 회원가입 뷰모델 완성및 Factory 패턴추가

---
+++ app/src/main/java/com/example/asteroid_impact/presentation/ui/login/RegisterViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.asteroid_impact.presentation.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.asteroid_impact.presentation.repository.FirebaseAuthRepository
+import kotlinx.coroutines.launch
+
+class RegisterViewModel(private val authRepository: FirebaseAuthRepository) : ViewModel() {
+
+    private val _registerResult = MutableLiveData<Result<Unit>>()
+    val registerResult: LiveData<Result<Unit>> = _registerResult
+
+    fun registerUser(email: String, password: String) {
+        viewModelScope.launch {
+            val result = authRepository.registerWithEmail(email, password)
+            _registerResult.value = result
+        }
+    }
+}
+
+class RegisterViewModelFactory(
+    private val authRepository: FirebaseAuthRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RegisterViewModel::class.java)) {
+            return RegisterViewModel(authRepository) as T
+        }
+        throw IllegalArgumentException("뷰모델 클래스가 없음")
+    }
+}
