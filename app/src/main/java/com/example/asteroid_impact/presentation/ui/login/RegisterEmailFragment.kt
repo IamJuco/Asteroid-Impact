a588ac6528878f534c4d3d7a88a6d66b3e53544f
Juseonghyun <wntjdgus99@naver.com>
2024-08-09 18:57:11 +0900

    feat: 이메일 인증만을 위한 Fragment 생성및 뷰바인딩 처리

---
+++ app/src/main/java/com/example/asteroid_impact/presentation/ui/login/RegisterEmailFragment.kt
@@ -0,0 +1,32 @@
+package com.example.asteroid_impact.presentation.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.asteroid_impact.R
+import com.example.asteroid_impact.databinding.FragmentRegisterEmailBinding
+
+class RegisterEmailFragment : Fragment() {
+    private var _binding: FragmentRegisterEmailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterEmailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
