3e3f1cbf9c94955216242cf1e3b9ca166f5a1fd6
Juseonghyun <wntjdgus99@naver.com>
2024-08-10 22:20:25 +0900

    feat: 로그인 실패에대한 예외처리

--- app/src/main/java/com/example/asteroid_impact/presentation/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/asteroid_impact/presentation/ui/login/LoginFragment.kt
@@ -2,6 +2,9 @@ package com.example.asteroid_impact.presentation.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +16,7 @@ import com.example.asteroid_impact.data.repository.FirebaseAuthRepositoryImpl
 import com.example.asteroid_impact.databinding.FragmentLoginBinding
 import com.example.asteroid_impact.presentation.ui.main.MainActivity
 import com.google.android.material.snackbar.Snackbar
+import java.util.regex.Pattern
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -34,6 +38,7 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setUpObserver()
         setUpListener()
+        setUpTextWatchers()
         backPressedForExitApp()
     }
 
@@ -44,9 +49,36 @@ class LoginFragment : Fragment() {
                 requireActivity().startActivity(Intent(requireActivity(), MainActivity::class.java))
                 requireActivity().finish()
             }.onFailure {
-                Snackbar.make(binding.root, "로그인 실패: ${it.message}", Snackbar.LENGTH_SHORT).show()
+                Log.d("0526LoginSuccessOrFailure", "로그인 실패 ${it.message}")
+                Snackbar.make(binding.root, "로그인에 실패했습니다. 이메일또는 비밀번호를 다시 확인해주세요.}", Snackbar.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun setUpTextWatchers() {
+        val textWatcher = object : TextWatcher {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val loginCheck = loginCheck()
+                binding.btnLogin.isEnabled = loginCheck
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+        }
+
+        binding.etEmail.addTextChangedListener(textWatcher)
+        binding.etPassword.addTextChangedListener(textWatcher)
+    }
+
+    private fun loginCheck(): Boolean {
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        val emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+        val emailPatternCheck = Pattern.matches(emailPattern, email)
+
+        return emailPatternCheck && email.isNotEmpty() && password.isNotEmpty()
     }
 
     private fun setUpListener() {
@@ -58,7 +90,7 @@ class LoginFragment : Fragment() {
         }
 
         binding.btnLogin.setOnClickListener {
-            val email = binding.etLogin.text.toString()
+            val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
             viewModel.loginUser(email, password)
         }
