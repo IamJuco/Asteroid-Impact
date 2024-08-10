260b84dbe7f1d8faaabd46fcc04c49a391028271
Juseonghyun <wntjdgus99@naver.com>
2024-08-10 14:35:53 +0900

    feat: 비밀번호 찾기 화면 추가및 바인딩 설정

---
+++ app/src/main/java/com/example/asteroid_impact/presentation/ui/login/FindPasswordVerifyFragment.kt
@@ -0,0 +1,31 @@
+package com.example.asteroid_impact.presentation.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.asteroid_impact.R
+import com.example.asteroid_impact.databinding.FragmentFindPasswordVerifyBinding
+
+class FindPasswordVerifyFragment : Fragment() {
+    private var _binding: FragmentFindPasswordVerifyBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFindPasswordVerifyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
