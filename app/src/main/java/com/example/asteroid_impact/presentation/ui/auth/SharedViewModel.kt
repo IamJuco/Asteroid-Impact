373dde1925d77e305a728eb01175fb0d24138f0a
Juseonghyun <wntjdgus99@naver.com>
2024-08-11 20:43:48 +0900

    style: 안쓰는 todo삭제

--- app/src/main/java/com/example/asteroid_impact/presentation/ui/auth/SharedViewModel.kt
+++ app/src/main/java/com/example/asteroid_impact/presentation/ui/auth/SharedViewModel.kt
@@ -162,7 +162,6 @@ class SharedViewModel(private val authRepository: FirebaseAuthRepository) : View
         }
     }
 
-    //TODO 앱 강제종료시 미리 회원가입된 정보를 삭제시켜야함
     fun tempAccountDeleteForDoNotEmailVerification() {
         val email = _email.value
         val password = Constants.USER_TEMP_PASSWORD
