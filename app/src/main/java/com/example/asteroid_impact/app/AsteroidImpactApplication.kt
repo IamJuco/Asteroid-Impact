782d4a3ba90df2db93ac6c2e6682093e07c4ec9b
Juseonghyun <wntjdgus99@naver.com>
2024-08-15 02:20:00 +0900

    feat: Application에서 Hilt 초기화

---
+++ app/src/main/java/com/example/asteroid_impact/app/AsteroidImpactApplication.kt
@@ -0,0 +1,8 @@
+package com.example.asteroid_impact.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AsteroidImpactApplication : Application() {
+}
